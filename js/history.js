76684233721c0c4760e174712cff04adfa83ae44
DAnomaly <parksh1219@naver.com>
2021-10-14 11:21:21 +0900

    history.json  오류수정

--- js/history.js
+++ js/history.js
@@ -7,19 +7,12 @@ function loadJson() {
     let section = $('#section').empty();
     if(section[0] == null) return;
     
-    $.getJSON("./config/history.json", "GET",
+    $.getJSON("./config/history.json", null,
         function (data, _textStatus, _jqXHR) {
-            console.log(2);
-            if(data.load == false) return;
-
-            $.each(data.datas, function (_index, element) { 
-                console.log(element);
-                console.log(element.key);
-                $div = $('<div>').addClass('');
-                $.each(element, function (indexInArray, valueOfElement) { 
-                    
-                });
+            $.each(collection, function (indexInArray, valueOfElement) { 
+                 
             });
         }
     );
+
 }
